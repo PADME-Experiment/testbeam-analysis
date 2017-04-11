f870f67bbf41526edb9d69a9d024f6ebccd9ca24
Georgi Georgiev <georgiev.georgi.st@gmail.com>
2017-04-11 14:24:20 +0200

    TB201704_Veto init

---
+++ TB201704_Veto/BTFCalorimeterChannel.hh
@@ -0,0 +1,21 @@
+#ifndef _BTFCalorimeterChannel_hh_
+#define _BTFCalorimeterChannel_hh_
+#include"VCalorimeterChannel.hh"
+class BTFCalorimeterChannel:public VCalorimeterChannel{
+  public:
+    BTFCalorimeterChannel(){
+      fGain=6.009615;
+      fSignalRangeBegin  =430;
+      fSignalRangeEnd    =600;
+      fNoiseRangeEnd     =fNoiseRangeBegin+fSignalRangeEnd-fSignalRangeBegin;
+      fOneParticleRangeBegin=50-12;
+      fOneParticleRangeEnd  =50+12;
+    }
+    void SetVoltage(unsigned v){
+      switch (v){
+        case 900: fGain=100;break;
+        default:throw "unknown voltage";
+      }
+    }
+};
+#endif
